fd6e349bbaf6da0850ac5f455c98d9c91f5e87da
Raffaella <raffaella.tran@dunnhumby.com>
2018-11-06 14:49:02 +0100

    Testing debugging and logs

--- app/src/main/java/com/example/rafaellat/journaler/Journaler.kt
+++ app/src/main/java/com/example/rafaellat/journaler/Journaler.kt
@@ -2,15 +2,30 @@ package com.example.rafaellat.journaler
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 class Journaler: Application() {
 
     companion object {
+        val tag = "Journaler"
         var ctx: Context? = null
     }
 
     override fun onCreate() {
         super.onCreate()
         ctx = applicationContext
+        Log.v(tag, "[ ON CREATE ]")
+    }
+
+    //triggered in critical memory situations (actively running processes should trim their memory usage)
+    override fun onLowMemory() {
+        super.onLowMemory()
+        Log.w(tag, "[ ON LOW MEMORY ]")
+    }
+
+    //memory is trimmed
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        Log.d(tag, "[ ON TRIM MEMORY ]: $level")
     }
 }
